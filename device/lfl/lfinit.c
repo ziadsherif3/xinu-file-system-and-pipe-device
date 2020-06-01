4bd920c2fc28dd0acf899cec211dceea080de33a
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-01 21:00:48 +0200

    files lfinit

--- device/lfl/lfinit.c
+++ device/lfl/lfinit.c
@@ -11,5 +11,9 @@ devcall	lfinit (
 	  struct dentry	*devptr	/* Entry in device switch table	*/
 	)
 {
+	struct lfcblk *lfptr; int32 i;
+	lfptr = &lftab[devptr->dvminor];
+	/* Initialize control block entry */
+
     return OK;
 }
