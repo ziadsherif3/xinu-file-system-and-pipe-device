d9f0467cfbb0535be7a24dff686cc63ef35c127a
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-02 13:49:19 +0200

    lfclose

--- device/lfl/lfclose.c
+++ device/lfl/lfclose.c
@@ -11,5 +11,25 @@ devcall	lfclose (
 	  struct dentry	*devptr	/* Entry in device switch table	*/
 	)
 {
+	struct lfcblk *lfptr; /* pointer to the open filetable entry */
+	lfptr = &lftab[devptr->dvminor];
+	wait(lfptr->lfmutex);
+	
+	/* if the file is not open return error */
+
+	if (lfptr->lfstate != USED) {
+		signal(lfptr->lfmutex);
+		return SYSERR;
+	}
+
+	/* Flush to disk if the data has been changed */
+	
+	if (lfptr->lfdbdirty) {
+		lfflush(lfptr);
+	}
+
+	/* free up the device and return */
+	lfptr->lfstate = FREE;
+	signal(lfptr->lfmutex);
     return OK;
 }
