73848ae555aa1edbf6263f856e3dad0e5b8356c3
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-08 14:30:59 +0200

    lfread_is_complete

--- device/lfl/lfread.c
+++ device/lfl/lfread.c
@@ -12,5 +12,29 @@ devcall	lfread (
       int32 count   /* Max bytes to read */
 	)
 {
-    return OK;
+    uint32 numRead; /* Number of Bytes read */
+    int32 nextByte; /* character or EOF or SYSERR */
+
+    if (count < 0) {
+        return SYSERR;
+    }
+    /* Use lfgetc to read bytes */
+    for ( numRead = 0; numRead < count; numRead++) {
+        nextByte = lflgetc(devptr);
+        if (nextByte == SYSERR) { 
+            return SYSERR;
+        } 
+        else if (nextByte == EOF) { /* unexpected EOF */
+            if (numRead == 0) {
+                return EOF;
+            }
+            else {
+                return numRead;
+            }
+        }
+        else {
+            *buff++ = (char) (0xff & nextByte);
+        }
+    }
+    return numRead;
 }
