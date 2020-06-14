d2e0cc0f415374bae2c9d524d8a7d809294929d0
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-14 02:46:49 +0200

    shell

---
+++ shell/xsh_mkdir.c
@@ -0,0 +1,12 @@
+/* xsh_mkdir.c - xsh_mkdir */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ * xhs_mkdir - Create a new Directory 
+ *------------------------------------------------------------------------
+ */
+
+shellcmd xsh_mkdir(int nargs, char *args[]) {
+    
+}
---
+++ shell/xsh_rmdir.c
@@ -0,0 +1,11 @@
+/* xsh_rmdir.c - xsh_rmdir */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ * xhs_rmdir - Delete an existing directory provided it's empty
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_echo(int nargs, char *args[]) {
+    
+}
