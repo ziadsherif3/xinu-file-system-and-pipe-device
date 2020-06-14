dff3c288c3e9a995d92f330ff46852f7cff76aee
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-14 22:22:07 +0200

    testing in progress

--- shell/xsh_rm.c
+++ shell/xsh_rm.c
@@ -8,14 +8,15 @@
  */
 shellcmd xsh_rm(int nargs, char *args[]) {
 
-    int32 i;
-    for (i = 0; i<nargs; i++) {
-        printf("%s\n",args[i]);
-    }
     if (nargs != 2) {
 		fprintf(stderr, "%s: invalid arguments\n", args[0]);
 		fprintf(stderr, "Usage: rm \"filePath\"\n");
 		return SYSERR;
     }
+
+    if ( (control(FSYSTEM, FDELETE,args[1],0)) == SYSERR) {
+      return SYSERR;
+    }
+    return 0;
     
 }
