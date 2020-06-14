d886af03cf8d514e2148fba0760fee637380513d
Mohamed Alharouni <mohamedharouni1@gmail.com>
2020-06-14 19:32:38 +0200

    testing in progress

--- shell/xsh_cat.c
+++ shell/xsh_cat.c
@@ -14,5 +14,18 @@ shellcmd xsh_cat(int nargs, char *args[]) {
 		fprintf(stderr, "Usage: cat \"filePath\"\n");
 		return SYSERR;
 	}
+	did32 fileDev;	/* File device descriptor */
+	char * readBuff;	/* buffer to hold read characters */
+	int32 numRead;	/* Number of Bytes read */
+
+	if ( (fileDev = open(FSYSTEM, args[1], "r") )  == SYSERR) { /*Cannot open file */
+		return SYSERR;
+	}
+
+	/* Read the file */
+	numRead = read(fileDev, readBuff, 100);
+	printf("%s\n", readBuff);
+	close(fileDev);
+	return 0;
 
 }
